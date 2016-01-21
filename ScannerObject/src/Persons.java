fa1ab4bdb02b80ee067e4fb3a449c300d14ddba9
slice <slice@localhost>
2016-01-21 17:37:48 -0500

    Homework

---
+++ ScannerObject/src/Persons.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Persons {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter Age in years: ");
+		int age = input.nextInt();
+		System.out.print("Enter weight in pounds: ");
+		double weight = input.nextDouble();
+		System.out.println("Your age and Weight");
+	}
+
+}
